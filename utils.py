725b5fdd18438e6ebc9f1440267f7e2b701a3b55
shizhe <654745845@qq.com>
2020-04-26 16:52:31 +0800

    drop last

--- utils.py
+++ utils.py
@@ -578,7 +578,7 @@ def get_data_loaders(dataset, data_root=None, augment=False, batch_size=64,
       rank=xm.get_ordinal(),
       shuffle=True)
     train_loader = DataLoader(train_set, batch_size=batch_size,
-                              sampler=sampler, **loader_kwargs)
+                              sampler=sampler, drop_last=True, **loader_kwargs)
   else:
     loader_kwargs = {'num_workers': num_workers, 'pin_memory': pin_memory,
                      'drop_last': drop_last} # Default, drop last incomplete batch
